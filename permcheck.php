e1887c5c3e936c1ab342a2da42dc01f6693e83fb
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2012-12-17 09:46:07 +0000

    I totally forgot to svn add permcheck.php

---
+++ permcheck.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Check if the user owns the message, return "admin" for admins
+ * @user: Which user to check (Use user_login)
+ * @msg_id: which msg to check
+ */
+ Class PermCheck Extends Bulletaeon {
+ function is_msg_owner ( $user, $msg_id ) {
+	 $msg = DBOperator::get_msg_by_id($msg_id);
+	 $msg = $msg[0];
+	 if ( $msg->msg_owner == $user )
+		 return true;
+	 else
+		 return false;
+ }
+ }
+
+?>
